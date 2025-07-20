54990d0abd160efae07c4716ced32bc1b9d9c7db
AdenA76 <97320937+AdenA76@users.noreply.github.com>
2025-07-20 19:03:50 -0400

    Added the Steam API to the file along with a .env file to store the api key seperately. A start for the project to make sure that the steam api works correctly.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#new changes
+import os
+from steam_web_api import Steam
+from dotenv import load_dotenv, dotenv_values
+load_dotenv()
+KEY = os.getenv("STEAM_API_KEY")
+
+steam = Steam(KEY)
+
+user_id = input("Enter your Steam ID:")
+user = steam.users.get_user_details(user_id)
+
+print(user)
